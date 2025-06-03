bf5093d85311d895dac6ac69b7d15d433a774729
Swanny <swanny@sw4n.dev>
2025-06-03 10:44:00 -0400

    fix: consistently being validated (#10)

--- pkg/builder/validation.go
+++ pkg/builder/validation.go
@@ -23,26 +23,53 @@ func (args *BuilderComponentArgs) Validate() error {
 
 // Validate validates the BuilderEnv
 func (env *BuilderEnv) Validate() error {
-	if env.BuilderPort == nil {
-		return fmt.Errorf("builder port is required")
+	if env.AuthTokenRefreshInterval == nil {
+		return fmt.Errorf("auth token refresh interval is required")
+	}
+	if env.AwsAccountId == nil {
+		return fmt.Errorf("aws account id is required")
+	}
+	if env.AwsAccessKeyId == nil {
+		return fmt.Errorf("aws access key id is required")
+	}
+	if env.AwsRegion == nil {
+		return fmt.Errorf("aws region is required")
+	}
+	if env.AwsSecretAccessKey == nil {
+		return fmt.Errorf("aws secret access key is required")
+	}
+	if env.BlockConfirmationBuffer == nil {
+		return fmt.Errorf("block confirmation buffer is required")
+	}
+	if env.BlockQueryCutoff == nil {
+		return fmt.Errorf("block query cutoff is required")
+	}
+	if env.BlockQueryStart == nil {
+		return fmt.Errorf("block query start is required")
+	}
+	if env.BuilderHelperAddress == nil {
+		return fmt.Errorf("builder helper address is required")
 	}
 	if env.BuilderKey == nil {
 		return fmt.Errorf("builder key is required")
 	}
+	if env.BuilderPort == nil {
+		return fmt.Errorf("builder port is required")
+	}
+	if env.BuilderRewardsAddress == nil {
+		return fmt.Errorf("builder rewards address is required")
+	}
+	if env.ChainOffset == nil {
+		return fmt.Errorf("chain offset is required")
+	}
+	if env.ConcurrentLimit == nil {
+		return fmt.Errorf("concurrent limit is required")
+	}
+	if env.HostChainId == nil {
+		return fmt.Errorf("host chain id is required")
+	}
 	if env.HostRpcUrl == nil {
 		return fmt.Errorf("host RPC URL is required")
-	}
-	if env.RollupRpcUrl == nil {
-		return fmt.Errorf("rollup RPC URL is required")
-	}
-	if env.ZenithAddress == nil {
-		return fmt.Errorf("zenith address is required")
-	}
-	if env.QuinceyUrl == nil {
-		return fmt.Errorf("quincey URL is required")
-	}
-	if env.OtelExporterOtlpEndpoint == nil {
-		return fmt.Errorf("otel exporter otlp endpoint is required")
 	}
 	if env.OauthAudience == nil {
 		return fmt.Errorf("oauth audience is required")
@@ -61,6 +88,21 @@ func (env *BuilderEnv) Validate() error {
 	}
 	if env.OauthTokenUrl == nil {
 		return fmt.Errorf("oauth token URL is required")
+	}
+	if env.OtelExporterOtlpEndpoint == nil {
+		return fmt.Errorf("otel exporter otlp endpoint is required")
+	}
+	if env.QuinceyUrl == nil {
+		return fmt.Errorf("quincey URL is required")
+	}
+	if env.RollupBlockGasLimit == nil {
+		return fmt.Errorf("rollup block gas limit is required")
+	}
+	if env.RollupChainId == nil {
+		return fmt.Errorf("rollup chain id is required")
+	}
+	if env.RollupRpcUrl == nil {
+		return fmt.Errorf("rollup RPC URL is required")
 	}
 	if env.RustLog == nil {
 		return fmt.Errorf("rust log is required")
@@ -85,6 +127,9 @@ func (env *BuilderEnv) Validate() error {
 	}
 	if env.TxPoolUrl == nil {
 		return fmt.Errorf("tx pool URL is required")
+	}
+	if env.ZenithAddress == nil {
+		return fmt.Errorf("zenith address is required")
 	}
 
 	return nil
