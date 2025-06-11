0c2d8e94e310f4c11db99eaaa652bc6a3741f6a9
Swanny <swanny@sw4n.dev>
2025-06-11 14:32:04 -0400

    fix: el env vars

--- pkg/ethereum/execution/types.go
+++ pkg/ethereum/execution/types.go
@@ -73,8 +73,52 @@ type ExecutionClientComponent struct {
 
 // ExecutionClientEnv contains environment variables for the execution client
 type ExecutionClientEnv struct {
-	// Add any environment variables needed by the execution client
+	// HOST_ZENITH_CONTRACT_ADDRESS - The address of the Host Zenith contract
+	HostZenithContractAddress pulumi.StringInput `pulumi:"hostZenithContractAddress"`
+	// RU_ORDERS_CONTRACT_ADDRESS - The address of the Rollup Orders contract
+	RuOrdersContractAddress pulumi.StringInput `pulumi:"ruOrdersContractAddress"`
+	// HOST_ORDERS_CONTRACT_ADDRESS - The address of the Host Orders contract
+	HostOrdersContractAddress pulumi.StringInput `pulumi:"hostOrdersContractAddress"`
+	// SIGNET_CHAIN_ID - The chain ID of the Signet network
+	SignetChainID pulumi.StringInput `pulumi:"signetChainID"`
+	// BLOB_EXPLORER_URL - The URL of the Blob Explorer
+	BlobExplorerUrl pulumi.StringInput `pulumi:"blobExplorerUrl"`
+	// SIGNET_STATIC_PATH - The path to the Signet static files
+	SignetStaticPath pulumi.StringInput `pulumi:"signetStaticPath"`
+	// SIGNET_DATABASE_PATH - The path to the Signet database
+	SignetDatabasePath pulumi.StringInput `pulumi:"signetDatabasePath"`
+	// RUST_LOG - The log level for the signet node
 	RustLog pulumi.StringInput `pulumi:"rustLog"`
+	// IPC_ENDPOINT - The IPC endpoint for the Signet client
+	IpcEndpoint pulumi.StringInput `pulumi:"ipcEndpoint"`
+	// RPC_PORT - The port for the JSON RPC service
+	RpcPort pulumi.StringInput `pulumi:"rpcPort"`
+	// WS_RPC_PORT - The port for the WebSocket RPC service
+	WsRpcPort pulumi.StringInput `pulumi:"wsRpcPort"`
+	// TX_FORWARD_URL - The URL for the transaction forwarder to send transactions to
+	TxForwardUrl pulumi.StringInput `pulumi:"txForwardUrl"`
+	// GENESIS_JSON_PATH - The path to the genesis JSON file
+	GenesisJsonPath pulumi.StringInput `pulumi:"genesisJsonPath"`
+	// HOST_ZENITH_DEPLOY_HEIGHT - The height of the Host Zenith contract deployment
+	HostZenithDeployHeight pulumi.StringInput `pulumi:"hostZenithDeployHeight"`
+	// BASE_FEE_RECIPIENT - The address of the base fee recipient
+	BaseFeeRecipient pulumi.StringInput `pulumi:"baseFeeRecipient"`
+	// HOST_PASSAGE_CONTRACT_ADDRESS - The address of the Host Passage contract
+	HostPassageContractAddress pulumi.StringInput `pulumi:"hostPassageContractAddress"`
+	// HOST_TRANSACTOR_CONTRACT_ADDRESS - The address of the Host Transactor contract
+	HostTransactorContractAddress pulumi.StringInput `pulumi:"hostTransactorContractAddress"`
+	// RU_PASSAGE_CONTRACT_ADDRESS - The address of the Rollup Passage contract
+	RuPassageContractAddress pulumi.StringInput `pulumi:"ruPassageContractAddress"`
+	// SIGNET_CL_URL - The URL of the consensus client http api to fetch blobs
+	SignetClUrl pulumi.StringInput `pulumi:"signetClUrl"`
+	// SIGNET_PYLON_URL - The URL of the pylon api to fetch blobs
+	SignetPylonUrl pulumi.StringInput `pulumi:"signetPylonUrl"`
+	// START_TIMESTAMP - The start timestamp for the signet node in epoch time
+	StartTimestamp pulumi.StringInput `pulumi:"startTimestamp"`
+	// SLOT_OFFSET - The slot offset for signet node in seconds
+	SlotOffset pulumi.StringInput `pulumi:"slotOffset"`
+	// SLOT_DURATION - The slot duration for the signet node in seconds
+	SlotDuration pulumi.StringInput `pulumi:"slotDuration"`
 }
 
 // GetEnvMap implements the utils.EnvProvider interface
