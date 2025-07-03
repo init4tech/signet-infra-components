d66af414bbc6d422a5b2797cc203947ecbeac86e
Swanny <swanny@sw4n.dev>
2025-07-03 08:50:12 -0400

    fix: fixes found from actually trying to use this code

--- pkg/signet_node/types.go
+++ pkg/signet_node/types.go
@@ -1,6 +1,8 @@
 package signet_node
 
 import (
+	"strconv"
+
 	"github.com/init4tech/signet-infra-components/pkg/utils"
 	crd "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apiextensions"
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
@@ -13,10 +15,26 @@ type AppLabels struct {
 	Labels pulumi.StringMap
 }
 
+// Public-facing structs with base Go types
 type SignetNodeComponentArgs struct {
 	Name                        string
-	Namespace                   pulumi.StringInput
+	Namespace                   string
 	Env                         SignetNodeEnv
+	ExecutionJwt                string
+	LighthousePvcSize           string
+	RollupPvcSize               string
+	ExecutionClientImage        string
+	ConsensusClientImage        string
+	ExecutionClientStartCommand []string
+	ConsensusClientStartCommand []string
+	AppLabels                   AppLabels
+}
+
+// Internal structs with Pulumi types for use within the component
+type signetNodeComponentArgsInternal struct {
+	Name                        string
+	Namespace                   pulumi.StringInput
+	Env                         signetNodeEnvInternal
 	ExecutionJwt                pulumi.StringInput
 	LighthousePvcSize           pulumi.StringInput
 	RollupPvcSize               pulumi.StringInput
@@ -43,7 +61,35 @@ type SignetNodeComponent struct {
 	SignetNodeVirtualService *crd.CustomResource
 }
 
+// Public-facing environment struct with base Go types
 type SignetNodeEnv struct {
+	HostZenithAddress             string `pulumi:"hostZenithAddress" validate:"required"`
+	RuOrdersContractAddress       string `pulumi:"ruOrdersContractAddress" validate:"required"`
+	HostOrdersContractAddress     string `pulumi:"hostOrdersContractAddress" validate:"required"`
+	SignetChainId                 int    `pulumi:"signetChainId" validate:"required"`
+	BlobExplorerUrl               string `pulumi:"blobExplorerUrl" validate:"required"`
+	SignetStaticPath              string `pulumi:"signetStaticPath" validate:"required"`
+	SignetDatabasePath            string `pulumi:"signetDatabasePath" validate:"required"`
+	RustLog                       string `pulumi:"rustLog"`
+	IpcEndpoint                   string `pulumi:"ipcEndpoint" validate:"required"`
+	RpcPort                       int    `pulumi:"rpcPort" validate:"required"`
+	WsRpcPort                     int    `pulumi:"wsRpcPort" validate:"required"`
+	TxForwardUrl                  string `pulumi:"txForwardUrl" validate:"required"`
+	GenesisJsonPath               string `pulumi:"genesisJsonPath" validate:"required"`
+	HostZenithDeployHeight        string `pulumi:"hostZenithDeployHeight" validate:"required"`
+	BaseFeeRecipient              string `pulumi:"baseFeeRecipient" validate:"required"`
+	HostPassageContractAddress    string `pulumi:"hostPassageContractAddress" validate:"required"`
+	HostTransactorContractAddress string `pulumi:"hostTransactorContractAddress" validate:"required"`
+	RuPassageContractAddress      string `pulumi:"ruPassageContractAddress" validate:"required"`
+	SignetClUrl                   string `pulumi:"signetClUrl" validate:"required"`
+	SignetPylonUrl                string `pulumi:"signetPylonUrl" validate:"required"`
+	HostStartTimestamp            int    `pulumi:"hostStartTimestamp" validate:"required"`
+	HostSlotOffset                int    `pulumi:"hostSlotOffset" validate:"required"`
+	HostSlotDuration              int    `pulumi:"hostSlotDuration" validate:"required"`
+}
+
+// Internal environment struct with Pulumi types
+type signetNodeEnvInternal struct {
 	HostZenithAddress             pulumi.StringInput `pulumi:"hostZenithAddress" validate:"required"`
 	RuOrdersContractAddress       pulumi.StringInput `pulumi:"ruOrdersContractAddress" validate:"required"`
 	HostOrdersContractAddress     pulumi.StringInput `pulumi:"hostOrdersContractAddress" validate:"required"`
@@ -69,19 +115,76 @@ type SignetNodeEnv struct {
 	HostSlotDuration              pulumi.StringInput `pulumi:"hostSlotDuration" validate:"required"`
 }
 
-// GetEnvMap implements the utils.EnvProvider interface
-// It creates a map of environment variables from the SignetNodeEnv struct
-func (e SignetNodeEnv) GetEnvMap() pulumi.StringMap {
+// Conversion function to convert public args to internal args
+func (args SignetNodeComponentArgs) toInternal() signetNodeComponentArgsInternal {
+	return signetNodeComponentArgsInternal{
+		Name:                        args.Name,
+		Namespace:                   pulumi.String(args.Namespace),
+		Env:                         args.Env.toInternal(),
+		ExecutionJwt:                pulumi.String(args.ExecutionJwt),
+		LighthousePvcSize:           pulumi.String(args.LighthousePvcSize),
+		RollupPvcSize:               pulumi.String(args.RollupPvcSize),
+		ExecutionClientImage:        pulumi.String(args.ExecutionClientImage),
+		ConsensusClientImage:        pulumi.String(args.ConsensusClientImage),
+		ExecutionClientStartCommand: pulumi.ToStringArray(args.ExecutionClientStartCommand),
+		ConsensusClientStartCommand: pulumi.ToStringArray(args.ConsensusClientStartCommand),
+		AppLabels:                   args.AppLabels,
+	}
+}
+
+// Conversion function to convert public env to internal env
+func (e SignetNodeEnv) toInternal() signetNodeEnvInternal {
+	return signetNodeEnvInternal{
+		HostZenithAddress:             pulumi.String(e.HostZenithAddress),
+		RuOrdersContractAddress:       pulumi.String(e.RuOrdersContractAddress),
+		HostOrdersContractAddress:     pulumi.String(e.HostOrdersContractAddress),
+		SignetChainId:                 pulumi.String(strconv.Itoa(e.SignetChainId)),
+		BlobExplorerUrl:               pulumi.String(e.BlobExplorerUrl),
+		SignetStaticPath:              pulumi.String(e.SignetStaticPath),
+		SignetDatabasePath:            pulumi.String(e.SignetDatabasePath),
+		RustLog:                       pulumi.String(e.RustLog),
+		IpcEndpoint:                   pulumi.String(e.IpcEndpoint),
+		RpcPort:                       pulumi.String(strconv.Itoa(e.RpcPort)),
+		WsRpcPort:                     pulumi.String(strconv.Itoa(e.WsRpcPort)),
+		TxForwardUrl:                  pulumi.String(e.TxForwardUrl),
+		GenesisJsonPath:               pulumi.String(e.GenesisJsonPath),
+		HostZenithDeployHeight:        pulumi.String(e.HostZenithDeployHeight),
+		BaseFeeRecipient:              pulumi.String(e.BaseFeeRecipient),
+		HostPassageContractAddress:    pulumi.String(e.HostPassageContractAddress),
+		HostTransactorContractAddress: pulumi.String(e.HostTransactorContractAddress),
+		RuPassageContractAddress:      pulumi.String(e.RuPassageContractAddress),
+		SignetClUrl:                   pulumi.String(e.SignetClUrl),
+		SignetPylonUrl:                pulumi.String(e.SignetPylonUrl),
+		HostStartTimestamp:            pulumi.String(strconv.Itoa(e.HostStartTimestamp)),
+		HostSlotOffset:                pulumi.String(strconv.Itoa(e.HostSlotOffset)),
+		HostSlotDuration:              pulumi.String(strconv.Itoa(e.HostSlotDuration)),
+	}
+}
+
+// GetEnvMap implements the utils.EnvProvider interface for internal env
+func (e signetNodeEnvInternal) GetEnvMap() pulumi.StringMap {
 	return utils.CreateEnvMap(e)
 }
 
 // ConsensusEnv contains environment variables for the consensus client
 type ConsensusEnv struct {
+	Example string `pulumi:"example"`
+}
+
+// Internal consensus env with Pulumi types
+type consensusEnvInternal struct {
 	Example pulumi.StringInput `pulumi:"example"`
 }
 
-// GetEnvMap implements the utils.EnvProvider interface
-func (e ConsensusEnv) GetEnvMap() pulumi.StringMap {
+// Conversion function for consensus env
+func (e ConsensusEnv) toInternal() consensusEnvInternal {
+	return consensusEnvInternal{
+		Example: pulumi.String(e.Example),
+	}
+}
+
+// GetEnvMap implements the utils.EnvProvider interface for internal consensus env
+func (e consensusEnvInternal) GetEnvMap() pulumi.StringMap {
 	return utils.CreateEnvMap(e)
 }
 
